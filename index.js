91d3ef350dd86e47400f504d6d94eb5a764b9205
NCMoseley <ncmoseley@gmail.com>
2018-01-05 10:03:50 -0800

    Remove Webpack LOL

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// import 'bootstrap';
-
-// var mydata = JSON.parse(data);
-// alert(mydata[0].name);
-// alert(mydata[0].age);
-// alert(mydata[1].name);
-// alert(mydata[1].age);
 
 
+
+$(function() {
+    
+   
+});
