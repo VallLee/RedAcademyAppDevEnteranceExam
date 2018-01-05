9d0d1b95317d1f9cbb24680af40c1bd88dde33b4
NCMoseley <ncmoseley@gmail.com>
2018-01-05 08:34:49 -0800

    Add jquery

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // import 'bootstrap';
 
+// var mydata = JSON.parse(data);
+// alert(mydata[0].name);
+// alert(mydata[0].age);
+// alert(mydata[1].name);
+// alert(mydata[1].age);
+
+
 function component() {
 
   var element = document.createElement('div');
