c0dbee908ff5bf15ebe67480a3e1f51e389abbc2
NCMoseley <ncmoseley@gmail.com>
2018-01-07 22:11:01 -0800

    Pull Questions from Array

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,15 @@
 
-
+var questionNumber = 0;
 
 $.getJSON('/js/quiz.json',).done(quizjson => {
+
+  $("button").on("click", function () {
+    let questionNumber = 0;
+      questionNumber = questionNumber + 1;
+
+      console.log(questionNumber);
+
+  });
 
 	console.log(quizjson.quizzes)
 
@@ -9,13 +17,12 @@ $.getJSON('/js/quiz.json',).done(quizjson => {
     $.getJSON('/js/quiz.json', function(data){
 
       let title = data.quizzes[0].title;
-      let question = data.quizzes[0].questions[0].question;
+      let question = data.quizzes[0].questions[questionNumber].question;
       for ( let i = 0 ; i < 4; i++ ) {
-      let answers = data.quizzes[0].questions[0].answers[i].content;
+      let answers = data.quizzes[0].questions[questionNumber].answers[i].content;
 
       $('#answer').append("<p>"+answers+" <button class='button' type='button' id="+i+">Select</button></p></br>");
     }
-
             $("h4").append(title);
             $("#question").append(question);
             $("#answer").append(answer);
