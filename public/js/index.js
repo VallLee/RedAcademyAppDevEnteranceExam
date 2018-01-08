b1edfdd10af45a677ba52f5c3a8909391ae2f6a4
NCMoseley <ncmoseley@gmail.com>
2018-01-07 21:44:34 -0800

    Pull Questions from Array

--- public/js/index.js
+++ public/js/index.js
@@ -5,20 +5,43 @@ $.getJSON('/js/quiz.json',).done(quizjson => {
 
 	console.log(quizjson.quizzes)
 
-  $("button").click(function(){
+  $("#quiz-one").click(function(){
     $.getJSON('/js/quiz.json', function(data){
 
-      let title = data.quizzes[1].title;
-      let question = data.quizzes[1].questions[0].question;
-      let answer = data.quizzes[1].questions[0].answers[2].content;
+      let title = data.quizzes[0].title;
+      let question = data.quizzes[0].questions[0].question;
+      for ( let i = 0 ; i < 4; i++ ) {
+      let answers = data.quizzes[0].questions[0].answers[i].content;
 
-        
+      $('#answer').append("<p>"+answers+" <button class='button' type='button' id="+i+">Select</button></p></br>");
+    }
+
             $("h4").append(title);
-            $("div").append(question);
-            $("div").append(answer);
-
+            $("#question").append(question);
+            $("#answer").append(answer);
 
         });
     });
+
+  $("#quiz-two").click(function(){
+    $.getJSON('/js/quiz.json', function(data){
+
+
+      let title = data.quizzes[1].title;
+      let question = data.quizzes[1].questions[0].question;
+      for ( let i = 0 ; i < 4; i++ ) {
+      let answers = data.quizzes[1].questions[0].answers[i].content;
+
+      $('#answer').append("<p>"+answers+" <button class='button' type='button' id="+i+">Select</button></p></br>");
+    }
+ 
+            $("h4").append(title);
+            $("#question").append(question);
+            $("#answer").append(answer);
+
+        });
+    });
+
+  
 });
 
