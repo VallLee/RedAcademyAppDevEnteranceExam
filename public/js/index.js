06632cc7ec52dda63b84c83a12a76f5b5ddc6551
NCMoseley <ncmoseley@gmail.com>
2018-01-07 19:35:00 -0800

    Get data to browser

--- public/js/index.js
+++ public/js/index.js
@@ -7,39 +7,17 @@ $.getJSON('/js/quiz.json',).done(quizjson => {
 	console.log(quizjson.quizzes)
 
   $("button").click(function(){
-    $.getJSON('/js/quiz.json', function(result){
-        $.each(result, function(question, field){
-            $("div").append(field + " ");
+    $.getJSON('/js/quiz.json', function(data){
+
+      let title = data.quizzes[1].title;
+      let question = data.quizzes[1].questions[0].question;
+
+        
+            $("div").append(title);
+            $("div").append(question);
+
+
         });
     });
 });
 
-// function () {
-//   select specific part of .json and append that to specific div ()
-
-//   reapeat x 3
-
-//   onclick.specific div function class that makes the original divs dissapear and the new divs appear with relevant Data. 
-
-// if the correct button is pressed, add 1 to the score. 
-
-// else the score stays the same. 
-
-// }
-// var score = 0;
-
-// function() {
-
-// when score reaches 3 display victory message. rinse and repeat for quiz 2. 
-
-// }
-
-// Mac, I will do my dambest in this class. I definietly have alot to learn, but I will put my heart and soul into this.program. 
-
-
-// Despite a quick drive through the mountains tomorrow, I intend to keep working on it until Monday morning. Please don't kick me off the team Coach, I can do this. 
-
-
-
-
-});
